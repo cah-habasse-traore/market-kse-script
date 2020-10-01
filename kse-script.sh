0f00e2c3018a2ba7507a20e061effd50ec5a55a9
habasse.traore <habasse.traore@loh001ct8vemd6m.local>
2020-09-30 23:29:16 -0400

    Pipeline Updates

---
+++ kse-script.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+set -e
+
+# kse-script.sh shell script is located in the ./kse-script/ folder
+# run the below command inside the Katalon Container to execute katalon script of the targeted tested application
+
+echo $PATH
+
+katalonc.sh -projectPath=./kse-script -retry=1 -retryStrategy=allExecutions -testSuiteCollectionPath="Test Suites/Market_TSC" -apiKey="5c150d5c-a512-41a2-a42c-48f4ec07560e" --config -webui.autoUpdateDrivers=true
